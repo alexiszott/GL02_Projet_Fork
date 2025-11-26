a52d1fa756fabbfed53b5824a59ac06d7f49c04c
Rodr0089 <rodrigues.gwendal@etudiant.univ-reims.fr>
2025-11-26 13:28:46 +0100

    feat(cours): Ajout du fichier cours.js.

---
+++ CruParser_A24_Student/cours.js
@@ -0,0 +1,13 @@
+var cours = function(index, type, capacite, horaire, jour, heureDeb, heureFin, semaine, salle){
+	this.index = index;
+	this.type = type;
+	this.capacite = capacite;
+	this.horaire = horaire;
+    this.jour = jour;
+    this.heureDeb = heureDeb;
+    this.heureFin = heureFin;
+    this.semaine = semaine;
+    this.salle = salle;
+}
+
+module.exports = cours;
